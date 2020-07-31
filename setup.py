4ad135b16219f23c854313ec770dd5d1735a3382
GrayAi0 <48690308+GrayAi0@users.noreply.github.com>
2020-07-31 19:34:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'cinput',
     packages = ['cinput'],
-    version = '1.0.0',
+    version = '1.0.2',
     description = 'cinput package',
     author = 'Gray',
     author_email = 'brhoom234567@gmail.com', # old email (:
     url = 'https://github.com/GrayAi0/cinput',
-    download_url = 'https://github.com/GrayAi0/cinput/archive/1.0.0.tar.gz',
+    download_url = 'https://github.com/GrayAi0/cinput/archive/1.0.2.tar.gz',
     keywords = ['cinput'],
     install_requires=['pythonnet'],
     classifiers = []
