71d9d4070c1f5d0d74da0d7a6d2d4fee85371d62
GrayAi0 <48690308+GrayAi0@users.noreply.github.com>
2020-07-31 05:21:27 +0300

    Update cinput.py

--- cinput.py
+++ cinput.py
@@ -1,26 +1,59 @@
 import sys, clr, colorama as color
-
+#--------------------------------
+#
+# cinput v1.0
+#
+# p1-words: List of words like ['command1', 'testcommand']
+#
+# p2-color: If entered word not in words list then will be colored to red or will be to green
+#
+# p3-ghostLetters: The entered word will be completed if is in words list
+#
+# p4-redenter: If word not in list and user press enter nothing will happen
+#
+#--------------------------------
 color.init()
-
 import System
 
-class cinput:
 
-    def __init__(self, words, color=True, ghostLetters=True, readenter=True):
-        self.words = words
+class EmptylistError(Exception):
+    '''if words list is empty this error will be thread'''
+    pass
+
+
+class cinput:
+    def __init__(self,
+                 words=[],
+                 color=True,
+                 ghostLetters=True,
+                 readenter=True):
+        '''
+         :arg: words List of words like ['command1', 'testcommand']
+
+         :color: If entered word not in words list then will be colored to red or will be to green
+
+         :ghostLetters: The entered word will be completed if is in words list
+         :redenter: If word not in list and user press enter nothing will happen
+
+         :return: self
+        '''
+
+
+
+        if len(words) < 1:
+            raise EmptylistError('can\'t continue with empty list')
+        self.words = list(dict.fromkeys(words))
         self.color = color
         self.ghostLetters = ghostLetters
         self.readenter = readenter
 
-
-
-    def writeGostWords(self ,gword):
+    def writeGostWords(self, gword):
         print(color.Fore.LIGHTBLACK_EX, end='')
         for letter in gword:
             print(letter, end='', sep='\r')
         print(color.Fore.RESET, end='')
 
-    def popstr(self ,word, lett):
+    def popstr(self, word, lett):
         word = list(word)
         if len(word) == 0:
             return ''
@@ -28,14 +61,14 @@ class cinput:
         return ''.join(word)
 
     def clearfore(self, lens='a'):
-        if lens == 'a':lens = len(max(self.words)) + 2
-        sys.stdout.write(' '*(lens))
-        sys.stdout.write('\b'*(lens))
+        if lens == 'a': lens = len(max(self.words)) + 2
+        sys.stdout.write(' ' * (lens))
+        sys.stdout.write('\b' * (lens))
 
     def clearlast(self, enter):
-        sys.stdout.write('\b'*(len(enter)))
-        sys.stdout.write(' '*(len(enter)))
-        sys.stdout.write('\b'*(len(enter)))
+        sys.stdout.write('\b' * (len(enter)))
+        sys.stdout.write(' ' * (len(enter)))
+        sys.stdout.write('\b' * (len(enter)))
 
     def readline(self, msg='.cInput>'):
         print(msg, end=' ')
@@ -43,16 +76,20 @@ class cinput:
         getword = False
         while True:
             key = System.Console.ReadKey()
-            if key.Key == 13 :
+            if key.Key == 13:
                 if self.readenter and enter in self.words:
                     break
                 elif not self.readenter:
                     break
                 else:
                     if getword:
-                        print(msg,color.Fore.GREEN+enter, end=color.Fore.RESET)
+                        print(msg,
+                              color.Fore.GREEN + enter,
+                              end=color.Fore.RESET)
                     else:
-                        print(msg,color.Fore.RED+enter, end=color.Fore.RESET)
+                        print(msg,
+                              color.Fore.RED + enter,
+                              end=color.Fore.RESET)
 
             if key.Key == 8 and enter != '':
                 sys.stdout.write(' ')
@@ -65,43 +102,51 @@ class cinput:
 
             if key.Key == 9:
                 self.clearlast(msg + ' ')
-                for k in range(8):
+                for _ in range(8):
                     sys.stdout.write('\b')
                     sys.stdout.write(' ')
                     sys.stdout.write('\b')
+                del _
                 cwords = []
                 for word in self.words:
                     if word.startswith(enter):
                         cwords.append(word)
                 if len(cwords) == 1:
                     enter = cwords[0]
-                    print(msg, color.Fore.GREEN+enter, end=color.Fore.RESET)
+                    print(msg, color.Fore.GREEN + enter, end=color.Fore.RESET)
                 elif len(cwords) > 1:
                     print(color.Fore.LIGHTBLUE_EX, end='')
                     for word in cwords:
-                        print(word, end=' '*4)
+                        print(word, end=' ' * 4)
                     print(color.Fore.RESET, end='')
-                    print('\n' + msg, color.Fore.GREEN+enter, end=color.Fore.RESET)
+                    print('\n' + msg,
+                          color.Fore.GREEN + enter,
+                          end=color.Fore.RESET)
                     if self.ghostLetters:
                         for word in self.words:
                             if word.startswith(enter):
                                 self.clearfore()
                                 self.writeGostWords(word.replace(enter, '', 1))
-                                sys.stdout.write('\b'*len(word.replace(enter, '', 1)))
+                                sys.stdout.write(
+                                    '\b' * len(word.replace(enter, '', 1)))
                                 break
                 else:
-                    print(msg, color.Fore.RED+enter, end=color.Fore.RESET)
-
+                    print(msg, color.Fore.RED + enter, end=color.Fore.RESET)
 
             if key.Key == System.ConsoleKey.Escape:
                 sys.stdout.write('\b')
                 sys.stdout.write(' ')
                 sys.stdout.write('\b')
 
-            if key.Key not in [13, 8, 9, 39, 37, 38, 409, 39, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 121,
-                               123, 91] and key.Key is not System.ConsoleKey.Escape:
+            if key.Key not in [
+                    13, 8, 9, 39, 37, 38, 409, 39, 112, 113, 114, 115, 116,
+                    117, 118, 119, 120, 121, 121, 123, 91
+            ] and key.Key is not System.ConsoleKey.Escape:
                 enter += key.KeyChar
-            elif key.Key in [38, 39, 37, 40, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 12, 91]:
+            elif key.Key in [
+                    38, 39, 37, 40, 112, 113, 114, 115, 116, 117, 118, 119,
+                    120, 121, 12, 91
+            ]:
                 sys.stdout.write('\b')
                 sys.stdout.write(' ')
                 sys.stdout.write('\b')
@@ -119,7 +164,8 @@ class cinput:
                     if word.startswith(enter):
                         self.clearfore()
                         self.writeGostWords(word.replace(enter, '', 1))
-                        sys.stdout.write('\b'*len(word.replace(enter, '', 1)))
+                        sys.stdout.write('\b' *
+                                         len(word.replace(enter, '', 1)))
                         getword = True
                         break
 
@@ -146,7 +192,7 @@ class cinput:
         return enter
 
 
-
 if __name__ == "__main__":
-    cin = cinput(['print', 'ensure', 'start', 'stop', 'System', 'Random', 'random'])
+    cin = cinput(
+        ['print', 'ensure', 'start', 'stop', 'System', 'Random', 'random'])
     cin.readline()
