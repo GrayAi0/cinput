f8de663d8b98e0cac1e5d4228857b248b69c5370
GrayAi0 <48690308+GrayAi0@users.noreply.github.com>
2020-07-31 14:15:31 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'cinput',
+    packages = ['cinput'],
+    version = '1.0.0',  # Ideally should be same as your GitHub release tag varsion
+    description = 'description',
+    author = 'Gray',
+    author_email = 'none@none.null',
+    url = 'https://github.com/GrayAi0/cinput_test/archive/1.0.0.tar.gz',
+    download_url = 'download link you saved',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
